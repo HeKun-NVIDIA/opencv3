56c93caade8683a4c4dc6202e91566ae634b7aaa
kunhe0512 <FEGTT@hekundeMacBook-Air.local>
2017-01-29 21:31:13 +0800

    20170129 test

---
+++ opencv3/opencv3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opencv3
+//
+//  Created by 何琨 on 17/1/29.
+//  Copyright (c) 2017年 何琨. All rights reserved.
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
